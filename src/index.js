10c086ebefa909cd22938717c3e060de83212a76
Aadarsh Kumar <123838450+Aadarsh032@users.noreply.github.com>
2024-04-27 13:26:44 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Ibutton from './Ibutton/Ibutton';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Ibutton />
   </React.StrictMode>
 );
 
